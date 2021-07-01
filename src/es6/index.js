cd617114dec08261109b9b61bf7e9eca6aa7b04e
devbug <hbran6@gmail.com>
2021-06-30 18:01:07 -0600

    LET, CONST, Multilinea, Propagacion y desestructuracion

--- src/es6/index.js
+++ src/es6/index.js
@@ -24,3 +24,54 @@ console.log(epicPhrase);
 // es6
 let epicPhrase2 = `${hello} ${world}`; //template literals `
 console.log(epicPhrase2);
+
+// MULTILINEAS
+
+// antes
+let lorem = "esto es un string chingon \n"
++ " y aqui sigue la linea";
+console.log(lorem);
+
+// es6
+let lorem2 = `esto es otro string chingon pero con es6
+y aqui sigue la frase con enter
+`;
+
+console.log(lorem2);
+
+// DESESTRUCTURACION
+
+// antes
+let person = {
+    'name': 'herber',
+    'age': 27,
+    'country': 'GT'
+};
+
+console.log(person.name, person.age, person.country);
+
+// es6
+let {name, age} = person;//asigna en variable el objeto
+console.log(name, age);
+
+// PROPAGACION
+let team1 = ['Herber', 'Andi', 'Sindy'];
+let team2 = ['Juan', 'Pedro', 'Pablo'];
+
+let education = ['Francisco', ...team1, ...team2]; // junta los dos arreglos con Francisco, debe ir un valor al inicio
+
+console.log(education);
+
+var hola = 'hola'; // de forma global
+let hola = 'hola'; // solo funciona en el scope o bloque
+
+{
+    var globalVar = "variable global"; //esto funciona fuera de los {}
+}
+
+{
+    let globalLet = 'let global';// esto solo funciona dentro de los {}
+}
+
+console.log(globalVar);
+console.log(globalLet);
