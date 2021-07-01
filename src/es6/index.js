92f6a9d53f2bf6939c109347766a2142e32fb998
devbug <hbran6@gmail.com>
2021-06-30 18:17:16 -0600

    Arrow functions, promises, parametros objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -77,4 +77,58 @@ console.log(globalVar);
 console.log(globalLet);
 
 const a = 'b';
-a = 'c';
+// a = 'c';
+console.log(a);
+
+// OBJETOS
+
+// antes
+let name = 'Herber';
+let age = 27;
+
+obj = {name: name, age: age};
+
+// es6
+obj2 = {name, age};
+
+console.log(obj);
+console.log(obj2);
+
+// ARROW FUNCTIONS
+const names = [
+    {name: 'Herber', age: 27},
+    {name: 'Sindy', age: 26}
+];
+
+let listOfNames = names.map(function(item){
+    console.log(item.name);
+});
+
+// es6
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age) => {
+    // codigo
+    console.log(name, age);
+}
+
+const listOfNames4 = name => {
+    console.log(name);
+}
+
+const square = num => num * num;
+
+// ASINCRONISMO, PROMESAS
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Ok');
+        }else{
+            reject('Not ok');
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
