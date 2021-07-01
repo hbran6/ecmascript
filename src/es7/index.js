81fe73078c6492483eb21e4dc766e3cd2170cf3e
devbug <hbran6@gmail.com>
2021-06-30 18:37:45 -0600

    Se agrega es7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// INCLUDES
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+if (numbers.includes(7)) {
+    console.log('Si hay 7');
+}else{
+    console.log('No hay 7');
+}
+
+// POTENCIAS
+
+let base = 4;
+let exponent = 4;
+let result = base **  exponent;
+console.log(result);
