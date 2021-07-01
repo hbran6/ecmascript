f7ced5906ca2374ab10a082f56253a8491542c37
devbug <hbran6@gmail.com>
2021-06-30 18:32:39 -0600

    Clases, modulos y generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -132,3 +132,39 @@ const helloPromise = () => {
 helloPromise()
     .then(response => console.log(response))
     .catch(error => console.log(error));
+
+// CLASES
+class calculator {
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+
+    sum(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import hello from './module';
+
+console.log(hello());
+
+// GENERATORS
+function* helloWorld(){
+    if(true){
+        yield 'Hello, ';
+    }
+    if (true) {
+        yield 'World';
+    }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
