008a4c8b5c99f7f5a2e2cbe21fbee4a61b71f9a4
devbug <hbran6@gmail.com>
2021-07-04 16:46:27 -0600

    Asyn y Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -24,3 +24,28 @@ console.log(values.length);
 const string = 'hello';
 console.log(string.padStart(8, 'hi'));
 console.log(string.padEnd(12, ' ----'));
+
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false) ? setTimeout(() => resolve('Hello World'), 3000) : reject(new Error('Test error'))
+    })
+};
+
+const HelloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+HelloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
